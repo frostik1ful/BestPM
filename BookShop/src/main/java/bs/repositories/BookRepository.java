f62014e7b8cf905892dbabb4b5f34c3745f72f02
Dima Tishchenko <frostik1ful@gmail.com>
2019-07-08 01:30:38 +0200

    c

--- BookShop/src/main/java/bs/repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package bs.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import bs.models.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-
-}
--- BookShop/src/main/java/bs/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package bs.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import bs.models.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-
-}
